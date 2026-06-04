d4d0f29f4e02e558f4faf331fef5ac6848ffd23b
alumno <alumno@Instituto.local>
2026-06-04 09:07:37 -0300

    04.06.26

--- TrabajoPractico5/app.js
+++ TrabajoPractico5/app.js
@@ -1,8 +1,47 @@
-let numero1 = 25;
-let numero2 = 23;
-let resultado
+let suma
+let resta 
+let multiplicacion
+let division
 
-resultado = numero1 + numero2;
+suma = 5 + 10
+resta = 16 - 10
+multiplicacion = 4 * 2 
+division = 30 / 5
 
-console.log(resultado);
-alert(resultado)
+alert(suma)
+alert(resta)
+
+console.log(multiplicacion)
+console.log(division)
+
+alert("El resultado de la suma de 5 + 10 es " +  suma)
+alert("El resultado de la resta de 16 - 10 es " +  resta)
+
+console.log("El resultado de la multiplicacion de 4 * 2 es " +  multiplicacion)
+console.log("El resultado de la division de 30 / 5 es " +  division)
+
+let nombreAlumno = "Raton"
+let apellidoAlumno = "Perez"
+let edadAlumno = 67
+
+console.log("El alumno " + nombreAlumno  +" "+ apellidoAlumno + " tiene " + edadAlumno + " años.")
+
+let precio1 = 10
+let precio2 = 8
+let precio3 = 20
+
+let total = precio1 + precio2 + precio3
+
+console.log("El total a pagar es "+ total)
+
+let comidaFavorita = "ñoquis"
+let colorFavorito = "amarillo"
+let numeroFavorito = "9"
+
+console.log("Mi comida favorita son los "+comidaFavorita+", mi color favorito es "+colorFavorito+ " y mi número favorito es "+numeroFavorito)
+
+let pais
+pais = "Argentina"
+console.log(pais)
+pais = "Brasil"
+console.log(pais)
