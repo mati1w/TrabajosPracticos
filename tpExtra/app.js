6154a932b53914d87aea166855a3d619865a00d8
mati1w <51085769@ivt.edu.ar>
2026-08-24 11:13:20 -0300

    24.08

---
+++ tpExtra/app.js
@@ -0,0 +1,11 @@
+let monto = 0
+let input = document.querySelector ("monto")
+let boton = document.querySelector ("boton")
+let resultado = document.querySelector ("descuento")
+
+boton.onclick = function (){
+monto = input.value
+if (monto => 50.000) {
+    
+}
+}
