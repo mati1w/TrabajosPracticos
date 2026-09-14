142ca321329275ffe22daaf1cc0f6653b0739c26
mati1w <51085769@ivt.edu.ar>
2026-09-14 11:16:52 -0300

    09.14.26

---
+++ actividadExtra/app.js
@@ -0,0 +1,19 @@
+let boton = document.querySelector ("#añadir")
+let sumario = document.querySelector ("#total")
+let parrafo = document.querySelector ("#parrafo")
+
+let galletitas = 0
+
+boton.onclick  = function (){
+galletitas = galletitas + 1
+sumario.textContent = galletitas
+
+if (galletitas <= 10){
+    parrafo.style.color = 'green'
+ } else if ((galletitas >= 10) && (galletitas <= 20)) {
+ parrafo.style.color = 'yellow'
+ } else (){
+     parrafo.style.color = 'red'
+ }
+}
+
